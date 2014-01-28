5e13b25241ae3d2411eb179960aac0fb396bf3c9
mjkersey <michael.kersey@gmail.com>
2014-01-27 21:09:31 -0500

    reorganize folders

--- unstable/bnr.sh
+++
@@ -1,7 +0,0 @@
-cd build/
-cmake ../sources/ -DCMAKE_INSTALL_PREFIX=`kde4-config --prefix` -DCMAKE_BUILD_TYPE=debugfull
-sudo make install
-kbuildsycoca4 2>/dev/null
-cd ..
-clear
-./run.sh
--- unstable/plasma-restart.sh
+++
@@ -1,4 +0,0 @@
-kquitapp plasma-desktop
-sleep 2
-plasma-desktop 2>/dev/null 1>/dev/null &
-
