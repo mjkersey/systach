e945fb74d43bde8a20aa9e8c2c634a7e6b7b3611
mjkersey <michael.kersey@gmail.com>
2014-01-29 00:19:39 -0500

    nightly

--- nightly/build/CMakeFiles/plasma_applet_systach_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "plasma_applet_systach_automoc.cpp"
-  "CMakeFiles/plasma_applet_systach_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/plasma_applet_systach_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
